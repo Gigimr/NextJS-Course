f07b6f1358f4aa59ad12f0a33d8939d3c45d540c
Gigi <gigi151521@gmail.com>
2024-11-22 17:50:45 -0600

    Implement user model and enhance authentication flow with MongoDB integration

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,18 +1,39 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
+import User from '@models/user';
+import { connectToDB } from '@utils/database';
+
 const handler = NextAuth({
-    providers: [
-      GoogleProvider({
-        clientId: process.env.GOOGLE_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      })
-    ],
-  async session(session) {},
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  async session(session) {
+    const sessionUser = await User.findOne({ email: session.user.email });
+    session.user.id = sessionUser_id.toString();
+    return session;
+  },
   async signIn({ profile }) {
-    try{
-        //serverless => lambda
-    }catch{}
+    try {
+      //serverless => lambda
+      await connectToDB();
+      const userExists = await User.findOne({ email: profile.email });
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          name: profile.name.replace(' ', '').toLowerCase(),
+          image: profile.picture,
+        }).save();
+      }
+
+      return true;
+    } catch (error) {
+      console.log('Error connecting to DB', error);
+      return false;
+    }
   },
 });
 
