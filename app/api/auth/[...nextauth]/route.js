3860bc6c235e7030479ff1e0971c9e752b7cfdc2
Gigi <gigi151521@gmail.com>
2024-11-26 15:54:58 -0600

    Refactor authentication callbacks to improve user session handling and streamline sign-in process

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -9,33 +9,39 @@ const handler = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
+    })
   ],
-  async session(session) {
-    // store the user id from MongoDB to session
-    const sessionUser = await User.findOne({ email: session.user.email });
-    session.user.id = sessionUser._id.toString();
-    return session;
-  },
-  async signIn({ profile }) {
-    try {
-      //serverless => lambda
-      await connectToDB();
-      const userExists = await User.findOne({ email: profile.email });
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          name: profile.name.replace(' ', '').toLowerCase(),
-          image: profile.picture,
-        }).save();
+  callbacks: {
+    async session({ session }) {
+      // store the user id from MongoDB to session
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
+
+      return session;
+    },
+    async signIn({ account, profile, user, credentials }) {
+      try {
+        await connectToDB();
+
+        // check if user already exists
+        const userExists = await User.findOne({ email: profile.email });
+
+        // if not, create a new document and save user in MongoDB
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true
+      } catch (error) {
+        console.log("Error checking if user exists: ", error.message);
+        return false
       }
+    },
+  }
+})
 
-      return true;
-    } catch (error) {
-      console.log('Error connecting to DB', error);
-      return false;
-    }
-  },
-});
-
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
